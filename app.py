a194570fc5104ec0cfa1da05b8bd2eb1afb063e5
HIMA2702 <nasreddinemsbi@gmail.com>
2025-03-15 19:33:58 +0000

    Update app.py

--- app.py
+++ app.py
@@ -1036,7 +1036,7 @@ class Application:
         st.sidebar.title("🌟Navigation")
         st.markdown(
             """<style>
-            [data-testid="stSidebar"]{background:linear-gradient(to bottom,#004080,#00274d);color:white;padding:20px;border-radius:10px;}
+            [data-testid="stSidebar"]{background:linear-gradient(to bottom,#008049,#00274d);color:white;padding:20px;border-radius:10px;}
             .sidebar-title{color:white !important;font-size:22px !important;font-weight:bold;text-align:center;}
             [data-testid="stRadio"]{background:rgba(255,255,255,0.2);border-radius:10px;padding:10px;color:white;}
             [role="radiogroup"] label div{background:#ff1744 !important;color:white !important;border-radius:10px;padding:10px;transition:0.3s;}
@@ -1728,6 +1728,9 @@ class Application:
 
                             output = io.BytesIO()
                             with pd.ExcelWriter(output, engine="xlsxwriter") as writer:
+                                import matplotlib.pyplot as plt
+
+                                
                                 historical_start = data["date"].min().date()
                                 historical_end = data["date"].max().date()
                                 df_dates = pd.DataFrame({
